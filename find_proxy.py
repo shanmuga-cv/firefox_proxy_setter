e2a828d81458fb9126903e8e161612fc00ea71f4
Shanmuga <shanmuga.chidambaravel@gmail.com>
2020-03-07 22:26:31 +0100

    added function to poke for proxy health

--- find_proxy.py
+++ find_proxy.py
@@ -1,33 +1,55 @@
 import logging
-logging.basicConfig(level=logging.DEBUG)
-
-logger = logging.getLogger("proxy_setter")
 from datetime import datetime, timedelta
 
+import requests
+
+from fireox_proxy_setter import set_proxy
 from proxy_scraper import ProxyRecord, ProxyScrapper
-from fireox_proxy_setter import set_proxy, unset_proxy
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger("proxy_setter")
+logger.setLevel(logging.DEBUG)
+
+
+def poke(proxy: ProxyRecord, url="https://www.youtube.com/", timeout_seconds=5) -> bool:
+    try:
+        requests.get(
+            url,
+            proxies={k: f'http://{proxy.ip}:{proxy.port}/' for k in ['http', 'https']},
+            timeout=timeout_seconds
+        )
+        return True
+    except (requests.exceptions.ProxyError, requests.exceptions.ConnectTimeout) as e:
+        logger.debug("poke failed for: %s", proxy)
+        return False
+
 
 def proxy_filter(proxy: ProxyRecord):
     if proxy.country in ["China", "Indonesia", 'Germany', 'Iran']:
         logger.debug("rejecting proxy for country: %s", proxy)
         return False
-    elif proxy.since_last_update() > timedelta(minutes=25):
-        logger.debug("rejecting proxy for last update: %s %s",(datetime.now() - proxy.last_updated_at), proxy)
+    elif proxy.since_last_update() > timedelta(minutes=60):
+        logger.debug("rejecting proxy for last update: %s %s", (datetime.now() - proxy.last_updated_at), proxy)
         return False
-    elif proxy.uptime_success < proxy.uptime_failure:
+    elif proxy.uptime_success < proxy.uptime_failure * 2:
         logger.debug("rejecting proxy for uptime ratio: %s", proxy)
         return False
     elif proxy.uptime_failure + proxy.uptime_success < 20:
         logger.debug("rejecting proxy for uptime count: %s", proxy)
         return False
-    elif (600<= proxy.uptime_success <750  and 200 <= proxy.uptime_failure <= 300):
+    elif (
+            600 <= proxy.uptime_success < 750 and
+            200 <= proxy.uptime_failure <= 300
+    ):
         logger.debug("rejecting proxy for suspicion: %s", proxy)
         return False
     else:
         return True
 
+
 for proxy in ProxyScrapper(35):
-    if not proxy_filter(proxy):
+    if not (proxy_filter(proxy) and poke(proxy)):
         continue
     else:
         logger.info(f"setting proxy {repr(proxy)}")
