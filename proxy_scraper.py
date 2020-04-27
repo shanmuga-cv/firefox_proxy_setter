1dac6890efc635821bce3456a3b2854796bdec64
Shanmuga <shanmuga.chidambaravel@gmail.com>
2020-04-27 03:19:31 +0200

    refactored code

--- proxy_scraper.py
+++ proxy_scraper.py
@@ -32,8 +32,34 @@ class ProxyRecord:
     def __eq__(self, other):
         return isinstance(other, type(self)) and (self.ip, self.port) == (other.ip, other.port)
 
-    @classmethod
-    def from_html(cls, html_element):
+
+class GatherProxyScrapper:
+    def __init__(self):
+        pass
+
+    def scrape(self):
+        pageIdx = 1
+        max_pages = None
+        while max_pages is None or pageIdx <= max_pages:
+            logger.info(f"making http call with pageIdx {pageIdx}")
+            response = requests.post("https://proxygather.com/proxylist/anonymity/?t=Elite", data={
+                'PageIdx': pageIdx, 'Type': 'elite'
+            })
+            element = html.fromstring(response.text)
+            if max_pages is None:
+                max_pages = self.calculate_max_page(element)
+            table_rows = element.xpath('//table[@id="tblproxy"]/tr[position()>2]')
+            for tr in table_rows:
+                yield self.from_html(tr)
+            pageIdx += 1
+
+    @staticmethod
+    def calculate_max_page(html_element):
+        last_page_idx_str = html_element.xpath('//div[@class="pagenavi"]/a[last()]/text()')[0]
+        return int(last_page_idx_str)
+
+    @staticmethod
+    def from_html(html_element):
         row_values = html_element.xpath("./td")
 
         last_update_row, ip_row, port_row, level_row, country_row, city_row, uptime_row, response_time_row = row_values
@@ -57,30 +83,4 @@ class ProxyRecord:
 
         response_time = int(re.search(r"(\d+)ms", response_time_row.xpath("./text()")[0]).group(1))
 
-        return cls(last_updated_at, ip, port, level, country, uptime_success, uptime_failure, response_time)
-
-
-class GatherProxyScrapper:
-    def __init__(self):
-        pass
-
-    def scrape(self):
-        pageIdx = 1
-        max_pages = None
-        while max_pages is None or pageIdx <= max_pages:
-            logger.info(f"making http call with pageIdx {pageIdx}")
-            response = requests.post("https://proxygather.com/proxylist/anonymity/?t=Elite", data={
-                'PageIdx': pageIdx, 'Type': 'elite'
-            })
-            element = html.fromstring(response.text)
-            if max_pages is None:
-                max_pages = self.calculate_max_page(element)
-            table_rows = element.xpath('//table[@id="tblproxy"]/tr[position()>2]')
-            for tr in table_rows:
-                yield ProxyRecord.from_html(tr)
-            pageIdx += 1
-
-    @staticmethod
-    def calculate_max_page(html_element):
-        last_page_idx_str = html_element.xpath('//div[@class="pagenavi"]/a[last()]/text()')[0]
-        return int(last_page_idx_str)
+        return ProxyRecord(last_updated_at, ip, port, level, country, uptime_success, uptime_failure, response_time)
